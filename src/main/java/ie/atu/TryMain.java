6076efb805abeba5bb44908466080bf9815192a8
Padraic O Loughlin <g00468865@atu.ie>
2025-10-20 09:42:38 +0100

    Added Try, Catch

--- src/main/java/ie/atu/TryMain.java
+++ src/main/java/ie/atu/TryMain.java
@@ -1,23 +1,22 @@
+//Padraic O Loughlin
+//20/10/2025
+//Lab 6
+
 package ie.atu;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TryMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = 0;
-        while (true) {
             System.out.print("Enter your age");
-            String text = sc.nextLine();
-            try {
+            String text = sc.nextLine().trim();
+            try{
                 age = Integer.parseInt(text);
                 System.out.println("Thanks! You entered age = " + age);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input" + e.getMessage());
             }
-        }
     }
-}
