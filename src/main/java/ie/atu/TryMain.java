633829b4d47cd4c0c3b90fc3421d9c628c6e8dfa
Padraic O Loughlin <g00468865@atu.ie>
2025-10-20 10:12:22 +0100

    Writing a line to a file (try/catch)

--- src/main/java/ie/atu/TryMain.java
+++ src/main/java/ie/atu/TryMain.java
@@ -4,19 +4,30 @@
 
 package ie.atu;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class TryMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int age = 0;
-            System.out.print("Enter your age");
-            String text = sc.nextLine().trim();
-            try{
-                age = Integer.parseInt(text);
-                System.out.println("Thanks! You entered age = " + age);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input" + e.getMessage());
-            }
+
+        System.out.println("Please enter file name: ");
+        String fileName = sc.nextLine().trim();
+
+        System.out.println("Please enter a name to save: ");
+        String name = sc.nextLine().trim();
+
+        try(PrintWriter out = new PrintWriter(new FileWriter(fileName, true)))
+        {
+            out.println(name);
+            System.out.println("Saved to" + fileName);
+        }
+        catch (IOException ex)
+        {
+            System.out.println("Could not write to file: " + ex.getMessage());
+        }
     }
+}
+
